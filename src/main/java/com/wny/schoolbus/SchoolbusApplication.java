7fa5842f398ef0ac13504d4643c96240d8c13154
Andrey <andrew.saltul@gmail.com>
2024-09-16 21:54:09 -0400

    First

---
+++ src/main/java/com/wny/schoolbus/SchoolbusApplication.java
@@ -0,0 +1,13 @@
+package com.wny.schoolbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolbusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wny/schoolbus/SchoolbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wny.schoolbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
