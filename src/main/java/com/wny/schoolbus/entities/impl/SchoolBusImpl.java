e761d578fa39d7c6803e7a99c3d077958df2a39d
Andrey <andrew.saltul@gmail.com>
2024-09-16 23:06:51 -0400

    Added Entites, interfaces

--- src/main/java/com/wny/schoolbus/entities/impl/SchoolBusImpl.java
+++ src/main/java/com/wny/schoolbus/entities/impl/SchoolBusImpl.java
@@ -2,9 +2,11 @@ package com.wny.schoolbus.entities.impl;
 
 import com.wny.schoolbus.entities.Vehicle;
 import com.wny.schoolbus.enums.BusType;
+import com.wny.schoolbus.enums.FuelType;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
@@ -21,6 +23,12 @@ public class SchoolBusImpl implements Vehicle {
     @Enumerated(EnumType.STRING)
     private BusType busType;
 
+    @Enumerated(EnumType.STRING)
+    private FuelType fuelType;
 
+    @Getter
+    @Setter
+    @OneToMany
+    private TerminalImpl terminal;
 
 }
--- src/main/java/com/wny/schoolbus/entities/impl/TerminalImpl.java
+++ src/main/java/com/wny/schoolbus/entities/impl/TerminalImpl.java
@@ -7,18 +7,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name="terminal")
 @Getter
 @Setter
 @AllArgsConstructor
 public class TerminalImpl implements Terminal {
 
     @Id
-    @Column(name="id")
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
-
-    @Column(name="name")
     private String name;
 
 }
